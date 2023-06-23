14faba4b29b3a387647462efe3fc5a1bffc79af8
hisham-mhh <mdhisham.dev@gmail.com>
2023-06-23 10:02:33 +0530

    .

--- 2235 Add Two Integers.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def sum(self, num1: int, num2: int) -> int: # "-> int" at the end of the line indicates that the method will return an integer
-        return num1 + num2
-
-x = Solution()
-y = x.sum(3, 4)
-print(y)
