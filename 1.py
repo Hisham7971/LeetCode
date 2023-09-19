723073a9dfffadb397f986a25574af4bf2e8973b
Hisham7971 <mdhisham.dev@gmail.com>
2023-09-19 19:55:55 +0530

    Two Sum

--- 1.py
+++ 1.py
@@ -1,12 +1,9 @@
 # Two Sum #
 
-nums = [2,7,11,15]
+nums = [2, 7, 11, 15]
 target = 9
 
 for i in range(len(nums)):
     for j in range(i+1, len(nums)):
         if nums[i] + nums[j] == target:
-            print([i,j])
-
-
-
+            print([i, j])
